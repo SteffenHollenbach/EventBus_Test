8da627ee1163d7ffc208fd589caa20ca213edca5
Steffen Hollenbach <steffenhollenbach@web.de>
2015-07-24 08:32:44 +0200

    Initial commit, still working EventBus

---
+++ app/src/main/java/com/example/steffen/eventbus_test/MyEvent.java
@@ -0,0 +1,14 @@
+package com.example.steffen.eventbus_test;
+
+/**
+ * Created by Steffen on 23.07.2015.
+ */
+public class MyEvent {
+
+    int lat;
+    int lng;
+
+    public MyEvent(int lat) {
+        this.lat = lat;
+    }
+}
